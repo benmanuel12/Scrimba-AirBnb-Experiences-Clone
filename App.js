797c6d4b0f8bddd7f7732ad608e8c5a5da448c54
Benjamin Manuel <contact.ben.manuel@gmail.com>
2023-12-29 16:57:01 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
